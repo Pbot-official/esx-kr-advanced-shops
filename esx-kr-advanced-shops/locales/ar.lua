2bef05a713e1206d3259fa7c766e542b5e25023d
ToGo <54911844+Pbot-official@users.noreply.github.com>
2021-04-29 20:49:40 +0200

    Update ar.lua
    Added the arabic tanslation

--- esx-kr-advanced-shops/locales/ar.lua
+++ esx-kr-advanced-shops/locales/ar.lua
@@ -1,5 +1,5 @@
 Locales['ar'] = {
-  ['press_to_open'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺢﺘﻔﻟ [~b~E~w~] ﻂﻐﺿﺍ',
+	['press_to_open'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺢﺘﻔﻟ [~b~E~w~] ﻂﻐﺿﺍ',
 	['press_to_rob'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺔﻗﺮﺴﻟ [E] ﻂﻐﺿﺍ',
 	['press_to_open_center'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺢﺘﻔﻟ [~b~E~w~] ﻂﻐﺿﺍ',
 	['name_shop'] = 'ما الذي تريد تسمية متجرك إليه?',
