434d812456d3bbc38289abb33478aa434f94956e
ToGo <54911844+Pbot-official@users.noreply.github.com>
2021-04-29 20:48:39 +0200

    Create ar.lua

---
+++ esx-kr-advanced-shops/locales/ar.lua
@@ -0,0 +1,10 @@
+Locales['ar'] = {
+  ['press_to_open'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺢﺘﻔﻟ [~b~E~w~] ﻂﻐﺿﺍ',
+	['press_to_rob'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺔﻗﺮﺴﻟ [E] ﻂﻐﺿﺍ',
+	['press_to_open_center'] = '<FONT FACE="A9eelsh">ﺮﺠﺘﻤﻟﺍ ﺢﺘﻔﻟ [~b~E~w~] ﻂﻐﺿﺍ',
+	['name_shop'] = 'ما الذي تريد تسمية متجرك إليه?',
+	['buy_shop'] = 'شراء متجر ',
+	['robbery_cancel'] = 'تم إلغاء السرقة (ذهبت بعيداً جداً)',
+	['set_price'] = 'حدد سعر ما تريد بيعه.',
+	['how_much'] = 'كم تريد أن تبيع?',
+}
